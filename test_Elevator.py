b04df36738e0f3dd45ed2f2e849ff7f5157e6be6
Denis <69689773+Denis-Dev-2020@users.noreply.github.com>
2021-11-25 03:53:23 +0200

    Update test_Elevator.py

--- test_Elevator.py
+++ test_Elevator.py
@@ -1,7 +1,10 @@
 import unittest
 from Elevator import Elevator
+from FileImporting import openJSON_BuildingFile
 
-example = {
+data = openJSON_BuildingFile("Ex1_Buildings/B1.json")
+example1 = data["_elevators"][0]
+example2 = {
       "_id": 0,
       "_speed": 0.5,
       "_minFloor": -2,
@@ -15,12 +18,19 @@ example = {
 class TestElevator(unittest.TestCase):
     def testArea(self):
         ###   Checks if elevator receives the right inputs and can actually create an object  ###
-        self.assertGreaterEqual(Elevator(example,100,1,1).ID_,0)
-        self.assertGreater(Elevator(example,100,1,1).Speed_,0)
-        self.assertGreater(Elevator(example,100,1,1).CloseTime_,0)
-        self.assertGreater(Elevator(example,100,1,1).OpenTime_,0)
-        self.assertGreater(Elevator(example,100,1,1).StartTime_,0)
-        self.assertGreater(Elevator(example,100,1,1).StopTime_,0)
-        self.assertTrue(int(Elevator(example,100,1,1).MinFloor_),int)
-        self.assertTrue(int(Elevator(example,100,1,1).MaxFloor_),int)
-
+        self.assertGreaterEqual(Elevator(example1,100,1,1).ID_,0)
+        self.assertGreater(Elevator(example1,100,1,1).Speed_,0)
+        self.assertGreater(Elevator(example1,100,1,1).CloseTime_,0)
+        self.assertGreater(Elevator(example1,100,1,1).OpenTime_,0)
+        self.assertGreater(Elevator(example1,100,1,1).StartTime_,0)
+        self.assertGreater(Elevator(example1,100,1,1).StopTime_,0)
+        self.assertTrue(int(Elevator(example1,100,1,1).MinFloor_),int)
+        self.assertTrue(int(Elevator(example1,100,1,1).MaxFloor_),int)
+        self.assertGreaterEqual(Elevator(example2,100,1,1).ID_,0)
+        self.assertGreater(Elevator(example2,100,1,1).Speed_,0)
+        self.assertGreater(Elevator(example2,100,1,1).CloseTime_,0)
+        self.assertGreater(Elevator(example2,100,1,1).OpenTime_,0)
+        self.assertGreater(Elevator(example2,100,1,1).StartTime_,0)
+        self.assertGreater(Elevator(example2,100,1,1).StopTime_,0)
+        self.assertTrue(int(Elevator(example2,100,1,1).MinFloor_),int)
+        self.assertTrue(int(Elevator(example2,100,1,1).MaxFloor_),int)
